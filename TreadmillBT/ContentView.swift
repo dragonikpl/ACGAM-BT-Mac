f526e29e1d06735c533b24814e8966df2128d776
Lukasz Domaradzki <lukasz.domaradzki@aspiresys.com>
2021-05-20 11:43:44 +0200

    Initial Commit

---
+++ TreadmillBT/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TreadmillBT
+//
+//  Created by Lukasz Domaradzki on 20/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
